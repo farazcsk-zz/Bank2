55b21f6b268edbc79339f8d7f29eeef2b0aa244e
Faraz Khan <farazcsk@gmail.com>
2015-12-11 11:41:19 +0000

    -Implemented list view, view transactions.

--- app/src/main/java/com/example/faraz_khan/bank2/Control.java
+++ app/src/main/java/com/example/faraz_khan/bank2/Control.java
@@ -6,12 +6,14 @@ package com.example.faraz_khan.bank2;
 
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
 
 public class Control {
+
 
     private static Scanner input = new Scanner(System.in);
     private static Control instance;
@@ -21,6 +23,7 @@ public class Control {
     int accNumGenerator;
     double amount;
     String name;
+
 
     ArrayList<BaseAccount> accounts = new ArrayList<BaseAccount>();
 
@@ -62,186 +65,184 @@ public class Control {
     public void createAccount(int option2, String name, int acc_num, int id) {
 
 
-
-
-                switch (option2) {
-                    case 1:
-                        accounts.add(new CurrentAccount(name, acc_num, id));
-                        break;
-                    case 2:
-                        accounts.add(new SavingsAccount(name, acc_num, id));
-                        break;
-                    case 3:
-                        accounts.add(new StudentAccount(name, acc_num, id));
-                        break;
-                    case 4:
-                        accounts.add(new BusinessAccount(name, acc_num, id));
-                        break;
-                    case 5:
-                        accounts.add(new SMBAccount(name, acc_num, id));
-                        break;
-                    case 6:
-                        accounts.add(new IRAccount(name, acc_num, id));
-                        break;
-                    case 7:
-                        accounts.add(new HighInterestAccount(name, acc_num, id));
-                        break;
-                    case 8:
-                        accounts.add(new IslamicAccount(name, acc_num, id));
-                        break;
-                    case 9:
-                        accounts.add(new PrivateAccount(name, acc_num, id));
-                        break;
-                    case 10:
-                        accounts.add(new LCRAccount(name, acc_num, id));
-                        break;
-                }
+        switch (option2) {
+            case 1:
+                accounts.add(new CurrentAccount(name, acc_num, id));
+                break;
+            case 2:
+                accounts.add(new SavingsAccount(name, acc_num, id));
+                break;
+            case 3:
+                accounts.add(new StudentAccount(name, acc_num, id));
+                break;
+            case 4:
+                accounts.add(new BusinessAccount(name, acc_num, id));
+                break;
+            case 5:
+                accounts.add(new SMBAccount(name, acc_num, id));
+                break;
+            case 6:
+                accounts.add(new IRAccount(name, acc_num, id));
+                break;
+            case 7:
+                accounts.add(new HighInterestAccount(name, acc_num, id));
+                break;
+            case 8:
+                accounts.add(new IslamicAccount(name, acc_num, id));
+                break;
+            case 9:
+                accounts.add(new PrivateAccount(name, acc_num, id));
+                break;
+            case 10:
+                accounts.add(new LCRAccount(name, acc_num, id));
+                break;
+        }
 
 
     }
 
 
     public void deposit(int acc_number, double amount) {
-                //Write the instruction to the user
+        //Write the instruction to the user
         //System.out.println("Enter account Number: ");
 //Convert the string the user enters to an int
         //  acc_num = Integer.parseInt(input.nextLine());
-                //Write instruction to the user
+        //Write instruction to the user
         //System.out.println("Enter deposit amount: ");
 //Convert the string entered by the user to a double
         // amount = Double.parseDouble(input.nextLine());
 
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number) {
-                        System.out.println(acc_number);
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum() == acc_number) {
+                System.out.println(acc_number);
 
-                        accounts.get(i).deposit(amount);
-                        accounts.get(i).addTransaction(new Date(), "Deposit", amount);
-                        System.out.println(amount);
-                    }
-                }
+                accounts.get(i).deposit(amount);
+                accounts.get(i).addTransaction(new Date(), "Deposit", amount);
+                System.out.println(amount);
+            }
+        }
     }
 
 
     public double displayBalance(int acc_number) {
-                //Write the instruction to the user
+        //Write the instruction to the user
         //System.out.println("Enter account Number: ");
 //Convert the string the user enters to an int
         //acc_number = Integer.parseInt(input.nextLine());
 
 
         for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number) {
-                        amount = accounts.get(i).get_balance();
-                        accounts.get(i).addTransaction(new Date(), "View Balance", amount);
+            if (accounts.get(i).getAccountNum() == acc_number) {
+                amount = accounts.get(i).get_balance();
+                accounts.get(i).addTransaction(new Date(), "View Balance", amount);
 
 
-                    }
-                }
+            }
+        }
 
         return amount;
     }
 
 
     public double withdraw(int acc_number, double amount) {
-                //Write the instruction to the user
-                System.out.println("Enter account Number: ");
+        //Write the instruction to the user
+        System.out.println("Enter account Number: ");
 //Convert the string the user enters to an int
         //acc_number = Integer.parseInt(input.nextLine());
-                //Write instruction to the user
-                System.out.println("Enter deposit amount: ");
+        //Write instruction to the user
+        System.out.println("Enter deposit amount: ");
 //Convert the string entered by the user to a double
         //amount = Double.parseDouble(input.nextLine());
 
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number) {
-                        if (accounts.get(i).get_acc_type() == "Current") {
-                            if (amount <= 500) {
-                                accounts.get(i).withdraw(amount);
-                                accounts.get(i).addTransaction(new Date(), "Withdraw", amount);
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum() == acc_number) {
+                if (accounts.get(i).get_acc_type() == "Current") {
+                    if (amount <= 500) {
+                        accounts.get(i).withdraw(amount);
+                        accounts.get(i).addTransaction(new Date(), "Withdraw", amount);
 
-                            } else {
-                                System.out.println("The maximum daily withdrawal for a Current account is ?500. This transaction has been cancelled");
-
-
-                            }
-                        } else if (accounts.get(i).get_acc_type() == "Savings") {
-                            if (amount <= 300) {
-                                accounts.get(i).withdraw(amount);
-                                accounts.get(i).addTransaction(new Date(), "Withdraw", amount);
-
-                            } else {
-                                System.out.println("The maximum daily withdrawal for a Savings account is ?300. This transaction has been cancelled");
-
-                            }
-                        } else if (accounts.get(i).get_acc_type() == "Business") {
-                            if (amount <= 500) {
-                                accounts.get(i).withdraw(amount);
-                                accounts.get(i).addTransaction(new Date(), "Withdraw", amount);
-
-                            } else {
-                                System.out.println("The maximum daily withdrawal for a Business account is ?500. This transaction has been cancelled");
-
-                            }
-                        }
+                    } else {
+                        System.out.println("The maximum daily withdrawal for a Current account is ?500. This transaction has been cancelled");
 
 
                     }
+                } else if (accounts.get(i).get_acc_type() == "Savings") {
+                    if (amount <= 300) {
+                        accounts.get(i).withdraw(amount);
+                        accounts.get(i).addTransaction(new Date(), "Withdraw", amount);
+
+                    } else {
+                        System.out.println("The maximum daily withdrawal for a Savings account is ?300. This transaction has been cancelled");
+
+                    }
+                } else if (accounts.get(i).get_acc_type() == "Business") {
+                    if (amount <= 500) {
+                        accounts.get(i).withdraw(amount);
+                        accounts.get(i).addTransaction(new Date(), "Withdraw", amount);
+
+                    } else {
+                        System.out.println("The maximum daily withdrawal for a Business account is ?500. This transaction has been cancelled");
+
+                    }
                 }
+
+
+            }
+        }
         return amount;
     }
 
 
     public void transferMoney(int acc_number, double amount, int acc_number2) {              //Write the instruction to the user
         //System.out.println("Enter account Number to transfer money FROM:");
-                //Convert the string the user enters to an int
+        //Convert the string the user enters to an int
         // acc_number = Integer.parseInt(input.nextLine());
-                //Write instruction to the user
+        //Write instruction to the user
         //System.out.println("Enter transfer amount: ");
 //Convert the string entered by the user to a double
         // amount = Double.parseDouble(input.nextLine());
         CareTaker careTaker = new CareTaker();
         int counter = 0;
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number) {
-                        counter++;
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum() == acc_number) {
+                counter++;
 
-                        if (accounts.get(i).get_balance() >= amount) {
-                            careTaker.add(accounts.get(i).saveMemento());
-                            accounts.get(i).withdraw(amount);
-                            accounts.get(i).addTransaction(new Date(), "Transfer", amount);
+                if (accounts.get(i).get_balance() >= amount) {
+                    careTaker.add(accounts.get(i).saveMemento());
+                    accounts.get(i).withdraw(amount);
+                    accounts.get(i).addTransaction(new Date(), "Transfer", amount);
 
-                        } else {
-                            System.out.println("There are insufficient funds to make this transfer");
-                            accounts.get(i).getStateFromMemento(careTaker.get(0));
+                } else {
 
-                        }
-                    } else if (counter == 0) { //if no valid account was found
-                        System.out.println("Account number not recognised.");
-                        accounts.get(i).getStateFromMemento(careTaker.get(0));
-                    }
+                    System.out.println("There are insufficient funds to make this transfer");
+                    accounts.get(i).getStateFromMemento(careTaker.get(0));
+
                 }
-
+            } else if (counter == 0) { //if no valid account was found
+                System.out.println("Account number not recognised.");
+                accounts.get(i).getStateFromMemento(careTaker.get(0));
+            }
+        }
 
 
         System.out.println("Enter account Number to transfer money TO: ");
-                //Convert the string the user enters to an int
+        //Convert the string the user enters to an int
         //  acc_number2 = Integer.parseInt(input.nextLine());
         int counterTwo = 0;
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number2) {
-                        counterTwo++;
-                        accounts.get(i).deposit(amount);
-                        accounts.get(i).addTransaction(new Date(), "Transfer", amount);
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum() == acc_number2) {
+                counterTwo++;
+                accounts.get(i).deposit(amount);
+                accounts.get(i).addTransaction(new Date(), "Transfer", amount);
 
-                    } else if (counterTwo == 0) { //if no valid account was found
-                        System.out.println("Account number not recognised. No funds weretransferred.");
-                        //accounts.get(acc_number).deposit(amount); //put funds back in first account /***2.rollback feature***/
-                        accounts.get(i).getStateFromMemento(careTaker.get(0));
-                    }
-                }
+            } else if (counterTwo == 0) { //if no valid account was found
+                System.out.println("Account number not recognised. No funds weretransferred.");
+                //accounts.get(acc_number).deposit(amount); //put funds back in first account /***2.rollback feature***/
+                accounts.get(i).getStateFromMemento(careTaker.get(0));
+            }
+        }
 
-                System.out.println("Payment has been successfully transferred");
+        System.out.println("Payment has been successfully transferred");
 
 
     }
@@ -249,58 +250,63 @@ public class Control {
 
     public void payInterest() {
 
-                for (int i = 0; i < accounts.size(); i++) {
-                    accounts.get(i).interest_rate();
-                }
-                System.out.println("Interest has been paid");
+        for (int i = 0; i < accounts.size(); i++) {
+            accounts.get(i).interest_rate();
+        }
+        System.out.println("Interest has been paid");
 
     }
 
 
     public void addAccountHolder(int acc_number, String name) {
-                System.out.println("Enter Account Number");
+        System.out.println("Enter Account Number");
         acc_number = Integer.parseInt(input.nextLine());
-                System.out.println("Enter Customer first and Last Name");
-                name = input.nextLine();
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number) {
-                        accounts.get(i).AddAccHolder(name, acc_num);
-                        break;
-                    }
-                }
+        System.out.println("Enter Customer first and Last Name");
+        name = input.nextLine();
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum() == acc_number) {
+                accounts.get(i).AddAccHolder(name, acc_num);
+                break;
+            }
+        }
 
 
     }
 
 
     public void showAccounts(int acc_number) {
-                System.out.println("Enter Account Number");
-                int _id = Integer.parseInt(input.nextLine());
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getID() == acc_number) {
-                        System.out.println(accounts.get(i).get_acc_type() + " " + accounts.get(i).getHolderName() + " " + accounts.get(i).get_balance());
+        System.out.println("Enter Account Number");
+        int _id = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getID() == acc_number) {
+                System.out.println(accounts.get(i).get_acc_type() + " " + accounts.get(i).getHolderName() + " " + accounts.get(i).get_balance());
 
-                    }
-                }
+            }
+        }
 
 
     }
 
 
-    public void showTransactions(int acc_number) {
-                System.out.println("Enter Account Number");
-                acc_num = Integer.parseInt(input.nextLine());
-                int j = 0;
-                for (int i = 0; i < accounts.size(); i++) {
-                    if (accounts.get(i).getAccountNum() == acc_number) {
-                        while (j < accounts.get(i).getTransactions().size()) {
-                            System.out.println(accounts.get(i).getTransactions().get(j).getType() + " " + accounts.get(i).getTransactions().get(j).getDate() + " " + accounts.get(i).getTransactions().get(j).getAmount());
-                            j++;
-                        }
+    public List<String> showTransactions(int acc_number) {
+        // System.out.println("Enter Account Number");
+        // acc_num = Integer.parseInt(input.nextLine())
+        List<String> transactionList = new ArrayList<>();
+        int j = 0;
 
-                    }
+        for (int i = 0; i < accounts.size(); i++) {
+            if (accounts.get(i).getAccountNum() == acc_number) {
+                while (j < accounts.get(i).getTransactions().size()) {
+
+
+                    transactionList.add(accounts.get(i).getTransactions().get(j).getType() + " " + accounts.get(i).getTransactions().get(j).getDate() + " " + accounts.get(i).getTransactions().get(j).getAmount());
+
+                    j++;
                 }
 
+            }
+        }
+        return transactionList;
 
     }
 
@@ -390,7 +396,7 @@ public class Control {
         if (counter == 0) { //if no valid account was found
             System.out.println("Account number not recognised.");
         }
-}
+    }
 }
 
 
--- app/src/main/java/com/example/faraz_khan/bank2/ViewTransactions.java
+++ app/src/main/java/com/example/faraz_khan/bank2/ViewTransactions.java
@@ -6,8 +6,17 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ViewTransactions extends AppCompatActivity {
+    int acc_number;
+    Control ctrl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +34,26 @@ public class ViewTransactions extends AppCompatActivity {
             }
         });
     }
+
+    public void viewTransactionsButton(View view) {
+        ctrl = Control.getInstance();
+
+        EditText account_number = (EditText) findViewById(R.id.acc_num_transactions);
+
+
+        acc_number = Integer.parseInt(account_number.getText().toString());
+        List<String> transactionList = new ArrayList<>();
+        transactionList = ctrl.showTransactions(acc_number);
+
+
+        ListAdapter tAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, transactionList);
+        ListView transactionListView = (ListView) findViewById(R.id.transaction_listview);
+
+        transactionListView.setAdapter(tAdapter);
+
+
+    }
+
+
 
 }
